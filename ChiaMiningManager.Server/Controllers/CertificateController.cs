1343b52951f4ed3353fbe04baecbe4598e7356ab
Playwo <eliaswolf2001@t-online.de>
2021-03-30 01:48:55 +0200

    Cleanup

--- ChiaMiningManager.Server/Controllers/CertificateController.cs
+++ ChiaMiningManager.Server/Controllers/CertificateController.cs
@@ -1,8 +1,6 @@
 ﻿using ChiaMiningManager.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.IO.Compression;
 using System.Threading.Tasks;
 
 namespace ChiaMiningManager.Controllers
@@ -19,15 +17,9 @@ namespace ChiaMiningManager.Controllers
         }
 
         [HttpGet("Ca")]
-        public async Task<IActionResult> GetCAKeysAsync([FromHeader(Name = "Authorization")] string token)
-        {
-            if (!await DbContext.Miners.AnyAsync(x => x.Token == token))
-            {
-                return Unauthorized();
-            }
-
-     
-            return PhysicalFile("/root/ca.zip", "application/zip");
-        }
+        public async Task<IActionResult> GetCAKeysAsync([FromHeader(Name = "Authorization")] string token) 
+            => !await DbContext.Miners.AnyAsync(x => x.Token == token)
+                ? Unauthorized()
+                : PhysicalFile("/root/ca.zip", "application/zip", "ca.zip");
     }
 }
