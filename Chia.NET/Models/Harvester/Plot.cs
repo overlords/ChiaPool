1b49abff57fb51142188487b909ea0449761f0a2
Playwo <eliaswolf2001@t-online.de>
2021-04-01 21:56:06 +0200

    Add size property to plot

--- Chia.NET/Models/Harvester/Plot.cs
+++ Chia.NET/Models/Harvester/Plot.cs
@@ -13,6 +13,9 @@ namespace Chia.NET.Models
         [JsonPropertyName("plot_public_key")]
         public string PublicKey { get; init; }
 
+        [JsonPropertyName("size")]
+        public int Size { get; init; }
+
         [JsonConstructor]
         public Plot()
         {
