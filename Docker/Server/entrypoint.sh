a181226e1376a442a3462661172f9b8b87261d6b
Playwo <eliaswolf2001@t-online.de>
2021-04-01 02:38:01 +0200

    add postgres connection_string nullcheck

--- Docker/Server/entrypoint.sh
+++ Docker/Server/entrypoint.sh
@@ -9,9 +9,10 @@ cd chia-blockchain
 chia init
 
 if [[ -z "${wallet_keys}" ]]; then
-  echo "You need to provide keys for the wallet!"
-  echo "generating new keys, add them to the node.env file (wallet_keys=\"24words\")"
-  chia keys generate_and_print
+	echo "You need to provide keys for the wallet!"
+	echo "generating new keys, add them to the node.env file (wallet_keys=\"24words\")"
+	chia keys generate_and_print
+	echo "Aborting..."
   exit 1
 fi
 if [[ -z "${wallet_address}" ]]; then
@@ -19,12 +20,19 @@ if [[ -z "${wallet_address}" ]]; then
 	echo "Your wallet address is the \"First wallet address\" in the following output. Add it to your node.env file (wallet_address=xch...)"
 	echo "${wallet_keys}" | chia keys add
 	chia keys show
+	echo "Aborting..."
 	exit 1
 fi
 if [[ -z "${farmer_keys}" ]]; then
 	echo "You need to provide keys for plotting!"
 	echo "generating new keys, add them to the node.env file (farmer_keys=\"24words\")"
 	chia keys generate_and_print
+	echo "Aborting..."
+	exit 1
+fi
+if [[ -z "${db_connection}" ]]; then
+	echo "You need to provide a postgres connection string!"
+	echo "Aborting..."
 	exit 1
 fi
 
