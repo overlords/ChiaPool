bcc88ba82b4b55b30ea02ea768ee2bcfa046e6c8
Playwo <eliaswolf2001@t-online.de>
2021-04-01 02:33:02 +0200

    Fix bash syntax

--- Docker/Server/entrypoint.sh
+++ Docker/Server/entrypoint.sh
@@ -6,20 +6,22 @@ cd chia-blockchain
 
 . ./activate
 
-if [ ${wallet_keys} == "" ]; then
+chia init
+
+if [[ -z "${wallet_keys}" ]]; then
   echo "You need to provide keys for the wallet!"
   echo "generating new keys, add them to the node.env file (wallet_keys=\"24words\")"
   chia keys generate_and_print
   exit 1
 fi
-if [ ${wallet_address} == ""]; then
+if [[ -z "${wallet_address}" ]]; then
 	echo "You need to provide a wallet address!"
 	echo "Your wallet address is the \"First wallet address\" in the following output. Add it to your node.env file (wallet_address=xch...)"
 	echo "${wallet_keys}" | chia keys add
 	chia keys show
 	exit 1
 fi
-if [ ${farmer_keys} == ""]; then
+if [[ -z "${farmer_keys}" ]]; then
 	echo "You need to provide keys for plotting!"
 	echo "generating new keys, add them to the node.env file (farmer_keys=\"24words\")"
 	chia keys generate_and_print
@@ -28,9 +30,6 @@ fi
 
 echo "${farmer_keys}" | chia keys add
 echo "${wallet_keys}" | chia keys add
-
-chia init
-echo "${farmer_keys}" | chia keys add
 
 for i in $(echo ${plot_dirs} | tr ";" "\n") 
 do
