464d2d87e8be2e841c453b0fb01aaefe4d60a468
Playwo <eliaswolf2001@t-online.de>
2021-04-07 21:23:31 +0200

    Fix order of chia commands

--- Docker/Plotter/entrypoint.sh
+++ Docker/Plotter/entrypoint.sh
@@ -5,9 +5,9 @@ cd ../chia-blockchain
 
 . ./activate
 
-curl https://${pool_host}:${manager_port}/Cert/Keys -H "Authorization: ${token}" | chia keys add
 chia init
 sed -i 's/localhost/127.0.0.1/g' ~/.chia/mainnet/config/config.yaml
+curl https://${pool_host}:${manager_port}/Cert/Keys -H "Authorization: ${token}" | chia keys add
 
 cd ../manager
 ./ChiaPool.Plotter
