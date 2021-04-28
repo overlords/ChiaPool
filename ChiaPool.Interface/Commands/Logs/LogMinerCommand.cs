a24fec480b6e3fe1b7f1111c53bc7547ec3d6570
Playwo <eliaswolf2001@t-online.de>
2021-04-29 01:42:46 +0200

    Rename Log Pool to Log Miner

---
+++ ChiaPool.Interface/Commands/Logs/LogMinerCommand.cs
@@ -0,0 +1,40 @@
+﻿using ChiaPool.Api;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+using System.Threading.Tasks;
+
+namespace ChiaPool.Commands
+{
+    [Command("Log Miner", Description = "Retrieves the latest pool log output")]
+    public class LogMinerCommand : ChiaCommand
+    {
+        private readonly MinerApiAccessor MinerAccessor;
+
+        public LogMinerCommand(MinerApiAccessor minerAccessor)
+        {
+            MinerAccessor = minerAccessor;
+        }
+
+        [CommandOption("lines", 'n', Description = "Amount of log lines. Default is 100")]
+        public ushort LineCount { get; set; } = 100;
+
+        protected override async Task ExecuteAsync(IConsole console)
+        {
+            var lines = await MinerAccessor.GetPoolLogAsync(LineCount);
+
+            if (lines.Length == 0)
+            {
+                await InfoAsync("--- No entries ---");
+                return;
+            }
+
+            await InfoAsync($"Showing the last {lines.Length} lines:");
+            await WriteLineAsync();
+
+            foreach (var line in lines)
+            {
+                await WriteLineAsync(line);
+            }
+        }
+    }
+}
